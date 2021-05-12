0e918a3fdcfbadacbfee10b052ec171bdd7b15af
arumdauo <83822359+arumdauo@users.noreply.github.com>
2021-05-12 19:54:14 +0200

    Update ConnectFour.java
    – Aggiunta la possibilità di inserire i nomi dei giocatori.
    – Aggiunta la stampa dei messaggi per la vincita e il pareggio.

--- src/playGame/ConnectFour.java
+++ src/playGame/ConnectFour.java
@@ -7,24 +7,32 @@ public class ConnectFour {
 	public static void main(String[] args) 
 	{
 			Board gb = new Board();
-			String name1 = "Ciccio";
+			Scanner in = new Scanner(System.in);
+			System.out.print(" Please enter your name: ");
+			String name1 = in.next();
 			Color color1 = new Color(255, 0, 0);
 			Token token1 = new Token(color1);
 			Player p1 = new Player(name1, token1);
 			
-			String name2 = "Bugo";
+			System.out.print(" Please enter your name: ");
+			String name2 = in.next();
 			Color color2 = new Color(0, 255, 0);
 			Token token2 = new Token(color2);
 			Player p2 = new Player(name2, token2);
-			
 			
 			Match match = new Match(gb, p1, p2);
 			
 			match.executeTurn();
 			
 			Board.printBoard();
+		
+			if (gb.isDraw())    // Se è finita con un pareggio
+			{
+			    	System.out.print(" The match ended in a draw.");
+			}
+			else if (gb.isWin())    // Se è finita con una vincita
+		    {
+		    	System.out.print(" The match ended with a win.");
+		    }	
 	}
-			
-	
-
 }
