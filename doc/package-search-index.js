0d820e4bd9fd81bdb98a377ab7445c2e95b6144b
Andrea Stella <73061173+endriu00@users.noreply.github.com>
2021-07-03 13:01:49 +0200

    Added javadoc for LoadTester

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"customException"},{"l":"gameComponents"},{"l":"gameGUI"},{"l":"playGame"},{"l":"saveAndLoad"},{"l":"testing"},{"l":"utilities"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"testing"}]
